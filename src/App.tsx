86239458dd13f433857a3b649150aa96fad8573b
Ilya Makshanov <Ilya.Makshanov@vaz.ru>
2025-08-20 18:36:48 +0400

    feat: add dropDown menu

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import { Layout } from './shared/Layout';
 import { Header } from './shared/Header';
 import { Content } from './shared/Content';
 import { CardsList } from './shared/CardsList/CardsList';
-import { DropDown } from './shared/DropDown';
-import { GenericList } from './GenericList/GenericList';
 
 function AppComponent() {
   return (
@@ -16,43 +14,6 @@ function AppComponent() {
       <Header />
       <Content>
         <CardsList />
-        <div style={{ padding: '40px' }}>
-          <br />
-          {/* v1 */}
-          {/* <DropDown button={<button>Test</button>}> */}
-          {/* 1. можем положить в DropDown какой-то список */}
-          {/* <ul>
-              <li>1</li>
-            </ul> */}
-
-          {/* 2. можем положить готовую карточку */}
-          {/* <CardsList /> */}
-
-          {/* 3. можем положить список с какой-то логикой нажатия */}
-          {/* <ul>
-              <li onClick={console.log}>Click me!</li>
-              <li>Din't click me!</li>
-            </ul> */}
-
-          {/* 4. можем положить ранее созданый компонент для списков GenericList 
-              в котором можем генерировать каким будет наш список */}
-          {/* <GenericList list={[{ id: `1`, text: `Hello list!` }]} /> */}
-          {/* </DropDown> */}
-
-          {/* 5. контролируемый и неконтролируемый компонент 
-            На примере input: 
-            - Если у него есть value и я могу управлять содержимым то он контролируемы и что бы
-            управлять им нужно состояние, нужен handler на onChange передоавать его в Input.
-            <input type='text' value={''} />
-            - Если нет value то компонент живёт сам по себе и сам контролирует содержимое value
-            <input type='text' />
-            Сделаем DropDown контролируемым
-          */}
-
-          <DropDown isOpen={false} onOpen={() => console.log('Opened')} onClose={() => console.log('Closed')} button={<button>Test</button>}>
-            <GenericList list={[{ id: `1`, text: `Hello list!` }]} />
-          </DropDown>
-        </div>
       </Content>
     </Layout>
   );
--- src/shared/DropDown/DropDown.tsx
+++ src/shared/DropDown/DropDown.tsx
@@ -5,53 +5,36 @@ import { noop } from '../../utils/js/noop';
 interface IDropdownProps {
   button: React.ReactNode;
   children: React.ReactNode;
-  isOpen?: boolean; // для управления компонентом
-  onOpen?: () => void; // callback для управления компонентом
-  onClose?: () => void; // callback для управления компонентом
+  isOpen?: boolean;
+  onOpen?: () => void;
+  onClose?: () => void;
 }
 
 export function DropDown({ button, children, isOpen, onOpen = noop, onClose = noop }: IDropdownProps) {
-  // v1
-  // const [isDropDownOpen, setIsDropDownOpen] = useState(false);
-
-  // v2
   const [isDropDownOpen, setIsDropDownOpen] = useState(isOpen);
-  /* добавили useEffect что бы в DevTools во вкладке Components можно было менять false на true
-    и сост-е компонента могло изменяться на лету */
+
   useEffect(() => setIsDropDownOpen(isOpen), [isOpen]);
-  // активируем callback
   useEffect(() => (isDropDownOpen ? onOpen() : onClose()), [isDropDownOpen]);
+
   const handleOpen = () => {
     if (isOpen === undefined) {
       setIsDropDownOpen(!isDropDownOpen);
+    } else {
+      if (isDropDownOpen) onClose();
+      if (!isDropDownOpen) onOpen();
     }
   };
 
-  // v1
-  // const dropDownBtn = <div onClick={()=>setIsDropDownOpen(!isDropDownOpen)}>{button}</div>;
-  // v2
-  const dropDownBtn = <div onClick={handleOpen}>{button}</div>;
-
-  let dropDownElem;
-
-  if (isDropDownOpen) {
-    dropDownElem = (
-      <div className={styles.listContainer}>
-        <div
-          className={styles.list}
-          onClick={() => {
-            setIsDropDownOpen(false);
-          }}>
-          {children}
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className={styles.container}>
-      {dropDownBtn}
-      {dropDownElem}
+      <div onClick={handleOpen}>{button}</div>
+      {isDropDownOpen && (
+        <div className={styles.listContainer}>
+          <div className={styles.list} onClick={() => setIsDropDownOpen(false)}>
+            {children}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
