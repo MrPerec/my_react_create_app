7729304f545f41acd49da7482486e32be2283134
Ilya Makshanov <Ilya.Makshanov@vaz.ru>
2025-10-13 23:13:06 +0400

    feat: добавлены тесты

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    '^.+.tsx?$': ['ts-jest', {}],
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: { '\\.(css)': 'identity-obj-proxy' },
+  preset: 'ts-jest',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
