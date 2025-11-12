aad4be290890de6febe7eb069df77eedaa409387
Ilya Makshanov <mrperec@mail.ru>
2025-11-12 15:16:17 +0400

    feat: 8-4 context v2

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,33 @@ import { Content } from './shared/Content';
 import { CardsList } from './shared/CardsList/CardsList';
 import { useToken } from './hooks/useToken';
 import { tokenContext } from './shared/context/tokenContext';
+// import { userContext } from './shared/context/userContext';
+import { UserContextProvider } from './shared/context/userContext';
 
 function AppComponent() {
   const [token] = useToken();
-  const { Provider } = tokenContext;
+  // Шаг 1.
+  // уберём диструктуризацию т.к. все контексты возвращают Provider
+  // const { Provider } = tokenContext;
+  // и будем брать Provider как метод нужного нам контекста
 
+  // Шаг 2.
+  /** после того как написали userContextProvider в userContext.tsx можно избавить от <userContext.Provider value={{}}>
+   * и сразу использовать userContextProvider обернув в него весь App
+   */
   return (
-    /** обернули в Provider и предали token теперь он доступен из контекста
-     * а из пропоа Header убрали*/
-    <Provider value={token}>
-      <Layout>
-        <Header />
-        <Content>
-          <CardsList />
-        </Content>
-      </Layout>
-    </Provider>
+    <tokenContext.Provider value={token}>
+      {/* <userContext.Provider value={{}}> */}
+      <UserContextProvider>
+        <Layout>
+          <Header />
+          <Content>
+            <CardsList />
+          </Content>
+        </Layout>
+      </UserContextProvider>
+      {/* </userContext.Provider> */}
+    </tokenContext.Provider>
   );
 }
 
--- src/shared/Header/SearchBlock/SearchBlock.tsx
+++ src/shared/Header/SearchBlock/SearchBlock.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './searchblock.css';
 import { UserBlock } from './UserBlock';
-import { useUserData } from '../../../hooks/useUserData';
 import { SearchInputBlock } from './SearchInputBlock';
 import { EmailBlock } from './EmailBlock';
+import { userContext } from '../../context/userContext';
 
-// удалил проп token
 export function SearchBlock() {
-  // сделали кастомный хук и вынесли всю логику получения информации о пользователе в него
-  // удалил проп token и сразу из хука useUserData получаем все данные для аватарки
-  const [data] = useUserData();
+  const { iconImg, name } = useContext(userContext);
 
   return (
     <div className={styles.searchBlock}>
-      <UserBlock avatarSrc={data?.iconImg} username={data?.name} />
+      <UserBlock avatarSrc={iconImg} username={name} />
       <SearchInputBlock />
       <EmailBlock />
     </div>
