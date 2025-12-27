8fb9b1f601246c24f86acd4224762ddaf452badf
Ilya Makshanov <mrperec@mail.ru>
2025-12-27 11:50:09 +0400

    feat: add logger

--- src/InitialState/InitialState.tsx
+++ src/InitialState/InitialState.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, Middleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import { rootReducer } from '../reducers/rootReducer';
@@ -13,7 +13,34 @@ interface IInitialWrapperProps {
   children?: React.ReactNode;
 }
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
+
+/** создадим middleware */
+const logger: Middleware = (store) => (next) => (action) => {
+  // просмотр вызова action
+  console.log('dispatching: ', action);
+  // вызываем next и перадём в него action что бы action дошёл до reducer
+  let returnValue = next(action);
+  // просмотреть результат выполнения работы следующего middleware
+  console.log('action after next 1: ', returnValue);
+
+  // в middleware можно изменять результат выполнения action, добавиться поле name
+  returnValue = next({ ...action, name: 'Hello middleware!' });
+  console.log('action after next 2: ', returnValue);
+};
+
+/** добавим цепочку middleware */
+const ping: Middleware = (store) => (next) => (action) => {
+  console.log('ping');
+  next(action);
+};
+const pong: Middleware = (store) => (next) => (action) => {
+  console.log('pong');
+  next(action);
+};
+
+/** вписали applyMiddleware() */
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, ping, pong)));
 
 export function InitialState({ children }: IInitialWrapperProps) {
   useEffect(() => {
