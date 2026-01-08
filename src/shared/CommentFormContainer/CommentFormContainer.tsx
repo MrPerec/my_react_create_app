8fcfae4ad4392f11e8dc26f4be04f88198727f3b
Ilya Makshanov <mrperec@mail.ru>
2026-01-08 20:44:40 +0400

    feat: add token to localstorage

--- src/shared/CommentFormContainer/CommentFormContainer.tsx
+++ src/shared/CommentFormContainer/CommentFormContainer.tsx
@@ -1,17 +1,20 @@
-import React, { ChangeEvent, FormEvent, useContext } from 'react';
-import { userContext } from '../../context/UserContext';
+import React, { ChangeEvent, FormEvent /* , useContext  */ } from 'react';
+// import { userContext } from '../../context/UserContext';
 import { useDispatch, useSelector } from 'react-redux';
 import { CommentForm } from '../CommentForm/CommentForm';
 import { updateComment } from '../../actions/commentsActions';
 import { RootState } from '../../reducers/rootReducer';
 import { CommentsState } from '../../reducers/commentsReducer';
+import { useUserData } from '../../hooks/useUserData';
 
 interface ICommentFormContainerProps {
   commentId: number;
 }
 
 export function CommentFormContainer({ commentId }: ICommentFormContainerProps) {
-  const userData = useContext(userContext);
+  // const userData = useContext(userContext);
+  const { userData } = useUserData();
+
   const comment = useSelector<RootState, CommentsState>((state) => state.comments);
 
   let commentIndex = comment.findIndex((element) => element.id === commentId);
