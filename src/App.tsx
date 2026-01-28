ecd8fccd3213dc2f6d74b9be495be13a2b15ccb5
Ilya Makshanov <mrperec@mail.ru>
2026-01-28 15:20:12 +0400

    feat: add rout for posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 import './normalize.css';
 import './main.global.css';
@@ -9,21 +9,36 @@ import { Content } from './shared/Content';
 import { CardsList } from './shared/CardsList/CardsList';
 import { Initial } from './Initial/Initial';
 import { PostsContextProvider } from './context/PostsContext';
-import { BrowserRouter, Link, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Post } from './shared/Post';
 
 function AppComponent() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []); // вызывается 1 раз
+
   return (
     <Initial>
-      <BrowserRouter>
-        <Layout>
-          <Header />
-          <Content>
-            <PostsContextProvider>
-              <CardsList />
-            </PostsContextProvider>
-          </Content>
-        </Layout>
-      </BrowserRouter>
+      {/* что бы не возникало ошибки сделалил условие при оборачивании приложения в роутер 
+      p.s. возник баг с дублированием key у карточек постов */}
+      {mounted && (
+        <BrowserRouter>
+          <Layout>
+            <Header />
+            <Content>
+              <PostsContextProvider>
+                <CardsList />
+                {/* теперь модальное окно <Post /> будем монтировать здесь при помощи <Route> */}
+                <Route path={'/posts/:id'}>
+                  <Post />
+                </Route>
+              </PostsContextProvider>
+            </Content>
+          </Layout>
+        </BrowserRouter>
+      )}
     </Initial>
   );
 }
--- src/shared/CardsList/Card/TextContent/Title/Title.tsx
+++ src/shared/CardsList/Card/TextContent/Title/Title.tsx
@@ -1,29 +1,37 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './title.css';
 import { Post } from '../../../../Post';
+import { Link } from 'react-router-dom';
 
 export interface ITitleProps {
   link: string;
   title: string;
 }
 
-export function Title({ link, title }: ITitleProps) {
+/* export function Title({ link, title }: ITitleProps) {
   const [isModelOpen, setIsModelOpen] = useState(false);
   const titleRef = useRef<HTMLHeadingElement>(null);
 
   return (
     <>
       <h2 className={styles.title} ref={titleRef} onClick={() => setIsModelOpen(true)}>
-        {/* <a
-          className={styles.postLink}
-          href={link}
-          target='_blank'
-          onClick={() => setIsModelOpen(true)}>
-          {title}
-        </a> */}
         {title}
       </h2>
       {isModelOpen && <Post titleRef={titleRef} onClose={() => setIsModelOpen(false)} />}
     </>
+  );
+} */
+
+export function Title({ link, title }: ITitleProps) {
+  // убираем состояние, заменим его роутом
+  // const [isModelOpen, setIsModelOpen] = useState(false);
+
+  return (
+    <h2 className={styles.title}>
+      {/* вместо <a></a> и {title} будем использовать "Link". Вместо "href" используем "to" с уканаимем URI, onClick убрали т.к. Link из роута сам сделат переключение */}
+      <Link to={`/posts/1`}>{title}</Link>
+    </h2>
+    //   <Post /> перенесём в <App />
+    //   {isModelOpen && <Post titleRef={titleRef} onClose={() => setIsModelOpen(false)} />}
   );
 }
